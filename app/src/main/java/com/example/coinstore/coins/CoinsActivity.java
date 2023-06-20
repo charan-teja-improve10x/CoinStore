334cc0786c9f24bf06b3880df2dc1c5527de2ea3
CHARAN TEJA <charan.teja@improve10x.com>
2023-06-20 15:38:25 +0530

    created a package and created new activity

---
+++ app/src/main/java/com/example/coinstore/coins/CoinsActivity.java
@@ -0,0 +1,16 @@
+package com.example.coinstore.coins;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.coinstore.R;
+
+public class CoinsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coins);
+    }
+}
