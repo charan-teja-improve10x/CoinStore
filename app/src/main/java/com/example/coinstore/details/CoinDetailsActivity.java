dd9674d941a46dd390e2b782c4b3f4a1b2b44ee2
CHARAN TEJA <charan.teja@improve10x.com>
2023-06-21 16:02:15 +0530

    Implemented dataBinding in CoinDetailsActivity

--- app/src/main/java/com/example/coinstore/details/CoinDetailsActivity.java
+++ app/src/main/java/com/example/coinstore/details/CoinDetailsActivity.java
@@ -1,18 +1,43 @@
 package com.example.coinstore.details;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 
-import com.example.coinstore.R;
+import com.example.coinstore.databinding.ActivityCoinDetailsBinding;
+import com.example.coinstore.model.Tag;
+import com.example.coinstore.model.Team;
+
+import java.util.List;
 
 public class CoinDetailsActivity extends AppCompatActivity {
 
-
+    private ActivityCoinDetailsBinding binding;
+    private TeamsAdapter teamsAdapter;
+    private TagsAdapter tagsAdapter;
+    private List<Team> teams;
+    private List<Tag> tags;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_coin_details);
+        binding = ActivityCoinDetailsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setupAdapter();
+        setupRecyclerView();
+    }
+
+    private void setupAdapter() {
+        tagsAdapter = new TagsAdapter(tags);
+        teamsAdapter = new TeamsAdapter(teams);
+    }
+
+    private void setupRecyclerView() {
+        binding.teamRv.setLayoutManager(new LinearLayoutManager(this));
+        binding.tagsRv.setLayoutManager(new GridLayoutManager(this, 3));
+        binding.teamRv.setAdapter(teamsAdapter);
+        binding.tagsRv.setAdapter(tagsAdapter);
     }
 }
